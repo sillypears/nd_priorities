ac038f0e35c27fcdda64c0fbecf2a53c7ba9f47e
sillypears <sillypears@users.noreply.github.com>
2020-05-14 19:47:18 -0400

    wowee

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 $(function() {
     $.getJSON('necrodancer.json', function(data) {
-        var content = "<table id=\"enemies\" class=\"table table-bordered\">";
+        var content = `<table id="enemies" class="table table-bordered">`;
         const width = $(document).width()
         const total = data.length
         const rows =  Math.floor(width/total)+1
-        $.each(data, function(e) {
-            if ((e%rows) == 0 ||  e == 0) {
-                content += "<tr>";
+        data.forEach(function(e, i) {
+            if ((i%rows) === 0 ||  i === 0) {
+                content += `<tr>`;
             }
-            let ttip = "Name: " + data[e]["name"] + "<br />Type: " + data[e]["type"] + "<br />Priority: " + data[e]["priority"];
-            content += "<td class=\"text-center\">" + data[e]["priority"] + "\n<img data-html=\"true\" data-toggle=\"tooltip\" title=\"" + ttip + "\" src=\"" + data[e]['sprite'] + "\"></td>"
-
-            if ( (e+1%rows) == 0 && e != 0) {
-                content += "</tr>"
+            let ttip = `Name: ${e.name} <br />Type: ${e.type} <br />Priority: ${e.priority}`;
+            content += `<td class="text-center"> ${e.priority}<img data-html="true" data-toggle="tooltip" title="${ttip}" src="${e.sprite}"></td>`
+            if ( (i+1%rows) === 0 && i !== 0) {
+                content += `</tr>`;
             }
         });
-       content += "</tr></table>"
-       $('div#content').html(content)
+       content += `</tr></table>`;
+       $('div#content').html(content);
        $('[data-toggle="tooltip"]').tooltip();
     });
 });
