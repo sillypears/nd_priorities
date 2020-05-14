c7676df09ac7cd1a6645f6a094d1b994bb1ba780
sillypears <sillypears@users.noreply.github.com>
2020-05-14 19:24:26 -0400

    converts nd xml to json

--- app.py
+++ app.py
@@ -10,14 +10,13 @@ def main():
     root = ET.parse(XMLF).getroot()
     enemies = {}
     for e in root[1]:
-        id = 0
         name = e.tag
         priority = 0
         e_type = 0
         sprite = ""
+        s_x = 0
+        s_y = 0
         for a in e.attrib:
-            if a == "id":
-                id = int(e.attrib[a])
             if a == "type":
                 e_type = int(e.attrib[a])
         for child in e:
@@ -26,10 +25,18 @@ def main():
                     priority = int(child.attrib["priority"])
             if child.tag == "spritesheet":
                 sprite = child.text
-        enemies[str(id)] = {'type': e_type, 'name': name, 'priority': priority, 'sprite': sprite}
-    o_enemies = OrderedDict(sorted(enemies.items(), key = lambda x: getitem(x[1], 'priority')))
+                s_x = child.attrib["frameW"]
+                s_y = child.attrib["frameH"]
+        if priority > 0:
+            enemies[sprite] = {'type': e_type, 'name': name, 'priority': priority, 'sprite': sprite, 'frameW': s_x, 'frameH': s_y}
+    enemies = OrderedDict(sorted(enemies.items(), key = lambda x: getitem(x[1], 'priority')))
+    print(enemies)
+    new_enemies = []
+    
+    for e in enemies:
+        new_enemies.append(enemies[e])
     with open("necrodancer.json", 'w') as w:
-        w.write(json.dumps(o_enemies))
+        w.write(json.dumps(new_enemies))
     return 0
 
 if __name__ == "__main__":
