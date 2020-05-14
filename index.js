7096b970885d0456b293f75b1de56d2f4463c58c
sillypears <sillypears@users.noreply.github.com>
2020-05-14 19:24:56 -0400

    the website and stuff

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 $(function() {
-    console.log('loaded')
-})
+    $.getJSON('necrodancer.json', function(data) {
+        var content = "<table id=\"enemies\" class=\"table table-bordered\">";
+        const width = $(document).width()
+        const total = data.length
+        const rows =  Math.floor(width/total)+1
+        $.each(data, function(e) {
+            if ((e%rows) == 0 ||  e == 0) {
+                content += "<tr>";
+            }
+            let ttip = "Name: " + data[e]["name"] + "<br />Type: " + data[e]["type"] + "<br />Priority: " + data[e]["priority"];
+            content += "<td class=\"text-center\">" + data[e]["priority"] + "\n<img data-html=\"true\" data-toggle=\"tooltip\" title=\"" + ttip + "\" src=\"" + data[e]['sprite'] + "\"></td>"
+
+            if ( (e+1%rows) == 0 && e != 0) {
+                content += "</tr>"
+            }
+        });
+       content += "</tr></table>"
+       $('div#content').html(content)
+       $('[data-toggle="tooltip"]').tooltip();
+    });
+});
